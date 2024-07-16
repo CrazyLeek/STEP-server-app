6654dcc161d58c3807a77bf21ee234d7682e3c9b
Clémence Landais <clemence.landais@etu.uca.fr>
2024-07-16 10:52:36 +0100

    WIP

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ def get_user_records(user_id):
         SELECT r.*, j.name as journey_name
         FROM Records r
         LEFT JOIN Journeys j ON r.journeyId = j.journeyId
-        WHERE r.userId = ?
+        WHERE j.userId = ?
         ORDER BY r.startDate DESC
     '''
     
